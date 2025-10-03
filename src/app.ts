ba0fe3b448f27230850abcfb530fe6ff01770eec
ankitkumar782 <akankit615@gmail.com>
2025-10-03 17:12:22 +0530

    prismaclient added

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,54 @@
 import express from 'express';
-import {Client } from 'pg';
+import { Client } from 'pg';
 import { Constantdata } from './Constant/Constant.js';
+import { PrismaClient } from '@prisma/client';
 
-
-
-const app=express();
+const app = express();
 
 app.use(express.json())
-
-const pg_client=new Client(process.env.postgresqlurl)
+const client = new PrismaClient()
+const pg_client = new Client(process.env.postgresqlurl)
 
 pg_client.connect();
 
-app.post('/signUp',async (req,res,next)=>{
-    const name=req.body.name;
-    const email=req.body.email;
-    const password=req.body.password
-    
+app.post('/signUp', async (req, res, next) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password
+
     // const data=`INSERT INTO users (username,password,email) VALUES ('${username}', '${password}' ,'${email}')`; sql injection
     await pg_client.query('begin')
-    const db_res=await pg_client.query(`INSERT INTO users (name,password,email) VALUES ($1,$2,$3) RETURNING id,name`,[name,password,email] );
-    const userid=db_res.rows[0].id
-    const last_db=await pg_client.query(`INSERT INTO todos(workname,time,userid) VALUES($1,$2,$3)`,['sleeping','10pm',userid])
+    const db_res = await pg_client.query(`INSERT INTO users (name,password,email) VALUES ($1,$2,$3) RETURNING id,name`, [name, password, email]);
+    const userid = db_res.rows[0].id
+    const last_db = await pg_client.query(`INSERT INTO todos(workname,time,userid) VALUES($1,$2,$3)`, ['sleeping', '10pm', userid])
     res.status(200).json(last_db)
     await pg_client.query('commit')
-   
+
 })
 
-app.get('/metadata',async (req,res,next)=>{
-    const id=req.query.id;
-   
-    
+app.get('/metadata', async (req, res, next) => {
+    const id = req.query.id;
+
+
     // const data=`INSERT INTO users (username,password,email) VALUES ('${username}', '${password}' ,'${email}')`; sql injection
     await pg_client.query('begin')
-    const db_res=await pg_client.query(`SELECT u.name,u.email,t.workname,t.time FROM users u FULL JOIN todos t ON u.id=t.userid WHERE u.id=$1`,[id] );
-   
+    const db_res = await pg_client.query(`SELECT u.name,u.email,t.workname,t.time FROM users u FULL JOIN todos t ON u.id=t.userid WHERE u.id=$1`, [id]);
+
     res.status(200).json(db_res.rows)
     await pg_client.query('commit')
-   
+
+})
+
+app.post('/prismaadd', async (req, res) => {
+    console.log("entry")
+    const data=await client.user.create({
+        data: {
+            email: req.body.email,
+            password: req.body.password,
+            age: 29
+        }
+    })
+    res.status(200).json({response:data})
 })
 
 export default app;
